f9b813e6839f9d7c322afcd7fdbe890fe2086cd7
shirhabeel <shirhabeel5185@gmail.com>
2023-10-19 23:08:18 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://avatars.githubusercontent.com/u/113059240?v=4"
+      alt="shirhabeel-awan"
+    />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Shirhabeel Awan</h1>
+      <p>
+        Diving into a MERN stack technology starting with React. I'm a student
+        of software engineering. I like to participate in open source projects
+        of web developement using JavaScript and I just enjoy the long bike
+        rides of random roads around my city.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="Git & GitHub" emoji="👶" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
     </div>
   );
 }
